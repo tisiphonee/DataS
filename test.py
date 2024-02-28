1449091e9216b2afd7a65386b9794d8ca54fb3ff
behradbina <122400323+behradbina@users.noreply.github.com>
2024-02-28 20:14:08 +0330

    Add files via upload

--- test.py
+++ test.py
@@ -17,12 +17,10 @@ driver.get(url)
 # Wait for the page to load
 time.sleep(0.5)
 
-# Parse the page source using BeautifulSoup
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 table = soup.find('tbody')
 rows = table.find_all('tr', limit=11)[1:]  # Exclude the header row
 
-# Define lists to store transaction data
 hash_list = []
 block_list = []
 age_list = []
@@ -31,18 +29,16 @@ to_list = []
 value_list = []
 txn_fee_list = []
 
-# Extract data from each row and append it to the lists
-for row in rows[:10]:  # Get data from the first 10 rows (last 10 blocks)
+for row in rows[:10]:
     columns = row.find_all('td')
-    hash_list.append(columns[0].text.strip())
-    block_list.append(columns[1].text.strip())
-    age_list.append(columns[2].text.strip())
-    from_list.append(columns[3].text.strip())
-    to_list.append(columns[5].text.strip())
-    value_list.append(columns[6].text.strip())
-    txn_fee_list.append(columns[7].text.strip())
+    hash_list.append(columns[1].text.strip())
+    block_list.append(columns[3].text.strip())
+    age_list.append(columns[5].text.strip())
+    from_list.append(columns[7].text.strip())
+    to_list.append(columns[9].text.strip())
+    value_list.append(columns[10].text.strip())
+    txn_fee_list.append(columns[11].text.strip())
 
-# Create a pandas DataFrame from the lists
 df = pd.DataFrame({
     'Hash': hash_list,
     'Block': block_list,
@@ -53,9 +49,6 @@ df = pd.DataFrame({
     'Txn Fee': txn_fee_list
 })
 
-# Close the web driver
+
 driver.quit()
-
-# Print the DataFrame
-
 df.to_csv('output.csv', index=False)
