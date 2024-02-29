757ae8f9d8d4a7e78035adf09aab28c102d6ea5d
GreenGOC <121631466+GreenGOC@users.noreply.github.com>
2024-03-01 00:56:25 +0330

    Update Data Sampling.py
    Added Plotting and stuff. Hope this works well ::::::))))))))))))))

--- Data Sampling.py
+++ Data Sampling.py
@@ -1,16 +1,18 @@
+from scipy.stats import skew, kurtosis, norm
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import norm
-import numpy as np
 import math
 import random
 
+SAMPLE_PERCENTAGE = 10
 
 df = pd.read_csv('random_dataset.csv')
-sample_size = len(df) // 10
-df_simple_sample = df.sample(n = sample_size, random_state=random.randint(1, 10000000))
+sample_size = len(df) // SAMPLE_PERCENTAGE
+df = df.sample(n = sample_size, random_state=random.randint(1, 10000000))
 
+df.to_csv("SRSoutput.csv")
 ## Data Cleaning : 
 df['Value'] = df['Value'].str.rstrip(' ETH').astype(float)
 df['Txn Fee'] = df['Txn Fee'].astype(float)
@@ -42,6 +44,62 @@ sns.histplot(df['Value'], bins=int(math.sqrt(len(df['Value']))), color='b', kde=
 plt.title('Value Distribution')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
+
+plt.tight_layout()
+plt.show()
+
+
+skewness_value = skew(df['Value'])
+kurtosis_value = kurtosis(df['Value'])
+
+print(f"Value Skewness Value: {skewness_value}")
+print(f"Value Kurtosis Value: {kurtosis_value}")
+
+skewness_value = skew(df['Txn Fee'])
+kurtosis_value = kurtosis(df['Txn Fee'])
+
+print(f"Value Skewness Txn Fee : {skewness_value}")
+print(f"Value Kurtosis Txn Fee: {kurtosis_value}")
+
+plt.figure(figsize=(8, 5))
+
+plt.subplot(1, 2, 1)
+sns.histplot(df['Txn Fee'], bins=int(math.sqrt(len(df['Txn Fee']))),kde=True, color='g', alpha=0.7)
+mu, std = df['Txn Fee'].mean(), df['Txn Fee'].std()
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std) 
+plt.plot(x, p, 'k', linewidth=3)
+plt.title('Txn Fee Distribution with Normal Fit')
+plt.xlabel('Txn Fee')
+
+plt.subplot(1, 2, 2)
+sns.histplot(df['Value'], bins=int(math.sqrt(len(df['Value']))),kde=True, color='b', alpha=0.7)
+mu, std = df['Value'].mean(), df['Value'].std()
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = norm.pdf(x, mu, std)
+plt.plot(x, p, 'k', linewidth=3)
+plt.title('Value Distribution with Normal Fit')
+plt.xlabel('Value')
+plt.ylim(bottom=0)
+
+plt.tight_layout()
+plt.show()
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
+
+sns.boxplot(x=df['Txn Fee'], ax=axes[0, 0])
+axes[0, 0].set_title('Box Plot - Txn Fee')
+
+sns.violinplot(x=df['Txn Fee'], ax=axes[1,0])
+axes[1,0].set_title('Violin Plot - Txn Fee')
+
+sns.boxplot(x=df['Value'], ax=axes[0, 1])
+axes[0, 1].set_title('Box Plot - Value')
+
+sns.violinplot(x=df['Value'], ax=axes[1, 1])
+axes[1, 1].set_title('Violin Plot - Value')
 
 plt.tight_layout()
 plt.show()
